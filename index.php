52060097fc6750845ca0a187e887be8b8ce5da6b
upendram91 <upendra1991@gmail.com>
2015-03-27 17:22:23 -0600

    uploaded the basic version

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>WD SMITH</title>
+<link href="CSS/styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+     <div id="wrapper">
+     	<div id="header">
+          <div id="navbar">
+          	<ul id="navlist">
+              <li><a>Home</a></li>
+              <li><a>About</a></li>
+              <li><a>Photo collection</a></li>
+              <li><a>Learn More</a></li>
+              <li><a>Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div id="content">
+        </div>
+        <footer>
+        	
+        	<div id="footer-notes row">
+            	<p>
+                	<a>University of Texas at Arlington</a>
+                   	<a>Special Collections</a>
+                    <a>702 Planetarium Place, Arlington, TX 76019. 8172723000 </a>
+                </p>
+            </div>
+        </footer>
+     </div> 
+  </body>
+</html>
