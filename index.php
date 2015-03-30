097abc0b5861ecd7a97832f14e1568af89ef74e6
UPENDRA <upendra1991@gmail.com>
2015-03-30 18:07:11 -0500

    added more pages and styling
    added search.php and results.php
    added header and footer styling
    added forms

--- index.php
+++ index.php
@@ -4,26 +4,40 @@
 <link href="CSS/styles.css" rel="stylesheet" type="text/css">
   </head>
   <body>
-     <div id="wrapper">
+     <div id="wrapper" align="center">
      	<div id="header">
           <div id="navbar">
+            <div id= "titlelogo"></div>
+            <div id="searchbar" align="right">
+              <form action="">
+                <input type="text" width="" placeholder="enter  the search text here">
+                <input type="submit" value="Click here to Search">
+              </form>
+              <a href="#">Advanced Search</a>
+            </div>
           	<ul id="navlist">
-              <li><a>Home</a></li>
-              <li><a>About</a></li>
-              <li><a>Photo collection</a></li>
-              <li><a>Learn More</a></li>
-              <li><a>Contact us</a></li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Photo collection</a></li>
+              <li><a href="#">Learn More</a></li>
+              <li><a href="#">Contact us</a></li>
             </ul>
           </div>
         </div>
-        <div id="content">
-        </div>
+        <section id="content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
         <footer>
         	
         	<div id="footer-notes row">
-            	<p>
-                	<a>University of Texas at Arlington</a>
-                   	<a>Special Collections</a>
+            	<p align="right">
+                	<a>University of Texas at Arlington</a><br>
+                   	<a>Special Collections</a><br>
                     <a>702 Planetarium Place, Arlington, TX 76019. 8172723000 </a>
                 </p>
             </div>
---
+++ results.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<form>
+	<fieldset>
+		<label>Full Text Search</label><input type="text" placeholder="Enter Search Term"><br>
+		<label>Results/page</label><select>
+			<option value="5">5</option>
+			<option value="10">10</option>
+			<option value="20">20</option>
+			<option value="40">40</option>
+			<option value="60">60</option>
+			<option value="80">80</option>
+			<option value="100">100</option>
+		</select>
+		<label>Sort items by</label><select>
+			<option value="1">relevance</option>
+			<option value="2">issue date</option>
+			<option value="3">submit date</option>
+			<option value="4">title</option>
+		</select>
+		<label>In Order</label><select>
+			<option value="1">Ascending</option>
+			<option value="2">Descending</option>
+		</select>
+	</fieldset>
+	<input type="submit" value="go">
+</form>
+
+
+
+</body>
+</html>
